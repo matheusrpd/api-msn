fb423597bb632165be8374db19667db22db93ad6
matheusrpd <matheusrpd@ufrn.edu.br>
2020-02-04 17:55:04 -0300

    Estrutura da aplicação criada.

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// Middleware para verificar se usuário está logado
+routes.use(authMiddleware);
+
+export default routes;
