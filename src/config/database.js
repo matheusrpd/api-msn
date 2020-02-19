cc3419095315c9acd6e26827f28505d997e656d7
matheusrpd <matheusrpd@ufrn.edu.br>
2020-02-19 16:12:48 -0300

    Ajuste para bd em deploy.

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,23 @@
 require('dotenv/config');
 
 module.exports = {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
